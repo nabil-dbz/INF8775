6652bdecf918c1bfac59d513730a6d70c9bbfdb7
Nabil Dabouz <nbl.dabouz@gmail.com>
2022-02-23 13:43:55 -0500

    Changer le nom du script et rajouter des le build

--- TP1/tp.sh
+++ TP1/tp.sh
@@ -6,13 +6,12 @@ Command: assignment
   Run the Skyline Problem Algorithm.
 
 Usage:
-  assignment -e <file-name> -a <algorithm-type> [-p] [-t]
+  ./tp.sh -e <file-name> -a <algorithm-type> [-p] [-t]
 
 Options:
   -h    [REQUIRED] Show this screen.
   -e    [REQUIRED] Specify output file path.
-  -a    [REQUIRED] Specify the type of algirthme {brute, recursive, cutoff}.
-  -c    [OPTIONAL] Specify the cutoff of the recursive algorithm.
+  -a    [REQUIRED] Specify the type of algirthme {brute, recursif, seuil}.
   -p    [OPTIONAL] If used, the results should be printed on the screen.
   -t    [OPTIONAL] If used, the execution time should be printed on the screen.
   "
@@ -22,7 +21,6 @@ display_results=0
 display_execution_time=0
 input_file_name=
 algorithm_type=
-algorithm_cutoff=1
 
 while getopts hpte:a:c: opt; do
   case $opt in
@@ -34,7 +32,6 @@ while getopts hpte:a:c: opt; do
     t) display_execution_time=1 ;;
     e) input_file_name=$OPTARG ;;
     a) algorithm_type=$OPTARG ;;
-    c) algorithm_cutoff=$OPTARG ;;
     *)
       echo "ERROR: Please make sure to use the right options."
       exit 1
@@ -46,11 +43,20 @@ if [ -z "$input_file_name" ] || [ -z "$algorithm_type" ]; then
   exit 1
 fi
 
+if ! [[ -f "$input_file_name" ]]; then
+  echo "The provided file path $input_file_name doesn't exist."
+  exit 1
+fi
+
 algo_type=
 case $algorithm_type in
   "brute") algo_type=0 ;;
-  "recursive") algo_type=1 ;;
-  "cutoff") algo_type=2 ;;
+  "recursif") algo_type=1 ;;
+  "seuil") algo_type=2 ;;
+  *) echo "ERRO: The type of algorithme $algo_type is not recongnized." && exit 1;;
 esac
 
-./runner "$input_file_name" "$display_execution_time" "$display_results" "$algo_type" "$algorithm_cutoff"
+make clean > tmp
+make >> tmp
+./runner "$input_file_name" "$display_execution_time" "$display_results" "$algo_type"
+rm tmp
