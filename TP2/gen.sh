c6da496c49907c704df1e6d57c611ba919266767
Nabil Dabouz <nbl.dabouz@gmail.com>
2022-03-09 11:41:18 -0500

    Ajout d'un option pour prendre en consideration un dossier de sortie

--- TP2/gen.sh
+++ TP2/gen.sh
@@ -10,6 +10,6 @@ for n in {100,500,1000,5000,10000,50000,100000}; do
 			printf $1; if ($2 < $3) print " "$2" "$3; else print " "$3" "$2;
 			printf $2; if ($1 < $3) print " "$1" "$3; else print " "$3" "$1;
 			printf $3; if ($1 < $2) print " "$1" "$2; else print " "$2" "$1;
-		}' > b${n}_${i}.txt
+		}' > "$1"b"${n}"_"${i}".txt
 	done
 done
